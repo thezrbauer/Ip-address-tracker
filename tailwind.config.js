b6ead07983ddd0e2cf5a406486a4c049755d4a7e
Zack Bauer <zack.bauer30@gmail.com>
2021-10-21 14:17:15 -0400

    Initializin Express and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: { 
+      transparent: 'transparent',
+      current: 'currentColor',
+      White:'#FFFFFF',
+      DarkGray: 'hsl(0, 0%, 59%)',
+      VeryDarkGray: 'hsl(0, 0%, 17%)',
+
+    },
+    fontFamily: {
+      'rubik': ['rubik, sans-serif']
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
